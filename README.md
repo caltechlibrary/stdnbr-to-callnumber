9314f3d50823db4a32fa99b20b1d4b98f89bbaa7
R. S. Doiel <rsdoiel@gmail.com>
2015-11-12 17:08:30 -0800

    renamed repo, updated script to search stdnbr field in OCLC API

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # issn-to-loc-callnumber
 
-A quick and dirty shell script that takes a list of ISSN (one ISSN per line) and outputs a tab delimited file of ISSN and any LOC call numbers discovered via the OCLC classify API.
+A quick and dirty shell script that takes an OCLC standard number (one per line) and outputs a tab delimited file of standard number, call number and title based on what was avialable at the OCLC classify API.
 
 ## requirements
 
--- stdnbr-to-callnumber.sh
+++ stdnbr-to-callnumber.sh
@@ -1,42 +1,46 @@
 #!/bin/bash
 
 if [ "$1" = "" ]; then
-   echo 'USAGE: bash stnnbr-to-callnumber.sh ISSN_LIST_FILE'
+   echo 'USAGE: bash stnnbr-to-callnumber.sh STDNBR_LIST_FILE'
    exit 1
 fi
 
 mkdir -p data
+# Get all the documents by STDNBR
+CSV_FILE="data.csv"
+STDNBR=""
+TITLE=""
+CALL_NO=""
 if [ -f "data.csv" ]; then
     rm data.csv
 fi
-
-# Get all the documents by ISSN
-CSV_FILE="data.csv"
-ISSN=""
-TITLE=""
-CALL_NO=""
 echo 'Standard_Number,CALL_NO,TITLE' > $CSV_FILE
-cat $1 | while read ISSN; do
+cat $1 | while read STDNBR; do
     # now generate the owi version of the file...
-    echo "Checking $ISSN"
-    DATA_FILE="data/$ISSN.xml"
-    curl -s --output $DATA_FILE http://classify.oclc.org/classify2/Classify?stdnbr=$ISSN
+    echo "Checking $STDNBR"
+    DATA_FILE="data/$STDNBR.xml"
+    echo "Output results to $DATA_FILE"
+    curl -s --output $DATA_FILE http://classify.oclc.org/classify2/Classify?stdnbr=$STDNBR
+    if [ ! -f "data/$STDNBR.xml" ]; then
+        echo "Can't find data/$SDNBR.xml"
+        exit 1
+    fi
     # Check first for physical Journal Call No.
-    OWI=$(xpath data/$ISSN.xml '//work[@itemtype="itemtype-jrnl"]/@owi' | cut -d \" -f 2)
+    OWI=$(xpath data/$STDNBR.xml '//work[@itemtype="itemtype-jrnl"]/@owi' | cut -d \" -f 2)
     if [ "$OWI" = "" ]; then
         # then check for digital Journal Call No.
-        OWI=$(xpath data/$ISSN.xml '//work[@itemtype="itemtype-jrnl-digital"]/@owi' | cut -d \" -f 2)
+        OWI=$(xpath data/$STDNBR.xml '//work[@itemtype="itemtype-jrnl-digital"]/@owi' | cut -d \" -f 2)
     fi
     # We've check both types of journals and should have either the hard copy or digital copy.
     if [ "$OWI" != "" ]; then
         echo "Found OWI: $OWI"
-        DATA_FILE="data/owi-$ISSN.xml"
-        echo " Looking up Call number for $ISSN in $DATA_FILE"
+        DATA_FILE="data/owi-$STDNBR.xml"
+        echo " Looking up Call number for $STDNBR in $DATA_FILE"
         curl -s --output $DATA_FILE http://classify.oclc.org/classify2/Classify?owi=$OWI
         TITLE=$(xpath $DATA_FILE '//editions/edition[1]/@title' | cut -d \" -f 2)
         CALL_NO=$(xpath $DATA_FILE '//lcc/mostPopular/@sfa' | cut -d \" -f 2)
         echo "Title: $TITLE, Call No: $CALL_NO"
-        echo "$ISSN,$CALL_NO,$TITLE" >> $CSV_FILE
+        echo "$STDNBR,$CALL_NO,$TITLE" >> $CSV_FILE
     fi
 
 done
