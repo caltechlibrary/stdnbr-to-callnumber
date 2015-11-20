c4beeea67138de6a9e9e8595c0ec0ea16a8dab26
R. S. Doiel <rsdoiel@gmail.com>
2015-11-19 16:07:01 -0800

    Removed prompt to remove file

--- stdnbr-to-callnumber.sh
+++ stdnbr-to-callnumber.sh
@@ -7,22 +7,12 @@ fi
 
 mkdir -p data
 # Get all the documents by STDNBR
-TAB_DELIMITED_FILE="data.txt"
+TAB_DELIMITED_FILE="data-out-final.txt"
 STDNBR=""
 TITLE=""
 CALL_NO=""
 if [ -f $TAB_DELIMITED_FILE ]; then
-    if [ "$2" = "" ]; then
-        echo -n "Remove? (y/N) "
-        read YES_NO
-    else
-        YES_NO=$2
-    fi
-    if [ "$YES_NO" = "Y" ] || [ "$YES_NO" = "y" ]; then
-        rm $TAB_DELIMITED_FILE
-        touch $TAB_DELIMITED_FILE
-        echo -e "STANDARD_NUMBER\tCALL_NO\tTITLE" >> $TAB_DELIMITED_FILE
-    fi
+    echo "Appending to $TAB_DELIMITED_FILE"
 else
     touch $TAB_DELIMITED_FILE
     echo -e "STANDARD_NUMBER\tCALL_NO\tTITLE" >> $TAB_DELIMITED_FILE
@@ -31,8 +21,8 @@ cat $1 | while read STDNBR; do
     # now generate the owi version of the file...
     echo "Checking $STDNBR"
     DATA_FILE="data/$STDNBR.xml"
-    echo "Output results to $DATA_FILE"
-    curl -s --output $DATA_FILE http://classify.oclc.org/classify2/Classify?stdnbr=$STDNBR
+    echo "Output results to [$DATA_FILE]"
+    curl -s --output "$DATA_FILE" "http://classify.oclc.org/classify2/Classify?stdnbr=$STDNBR"
     if [ ! -f "data/$STDNBR.xml" ]; then
         echo "Can't find data/$STDNBR.xml"
         exit 1
